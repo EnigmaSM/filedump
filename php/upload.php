ea7887ef03bd2b9621baee2af6e8b82c196606a0
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2013-11-09 10:40:16 -0500

    WELCOME TO MY HEL

--- php/upload.php
+++ php/upload.php
@@ -9,39 +9,7 @@
 		<div id="intro">
 			<h1>Upload You File<span>(pls)</span></h1>
 		<?php
-			ini_set('display_errors', 'On');
-			if(isset($_FILES['upfile'])){
-				$file = $_FILES['upfile'];
-				$filedir = getenv("OPENSHIFT_DATA_DIR");
-				$newloc = $filedir . $file['name'];
-				move_uploaded_file($file['tmp_name'], $newloc);
-
-				
-				$dbloc = getenv("OPENSHIFT_MYSQL_DB_HOST");
-				$dbusr = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-				$dbpass = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-				$dbport = getenv("OPENSHIFT_MYSQL_DB_PORT");
-				/*
-				$dbloc = "localhost";
-				$dbusr = "root";
-				$dbpass = "root";
-				*/
-				$link = mysql_connect($dbloc, $dbusr, $dbpass);
-				mysql_select_db("filedump");
-
-				$numfiles = mysql_fetch_assoc(mysql_query(
-					"SELECT MAX(id) FROM files"
-					))["MAX(id)"];
-
-				if(!mysql_query(
-					"INSERT INTO files (id, filepath, elo) VALUES (". ($numfiles+1) .", '". $newloc ."', 1400)"
-					)){
-					echo("<p>". mysql_error() ."</p>");
-				}
-				echo('<p>File Submitted</p>');
-			} else{
-				echo('<p>Or don\'t. Free country.</p>');
-			}
+			
 		?>
 		
 		</div>
